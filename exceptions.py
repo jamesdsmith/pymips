e1fc6afafc8cf0fadd852f2a35965a5fc8133e8f
James Smith <codefiend@gmail.com>
2016-07-16 12:33:34 -0700

    All exception messages use string formatting

--- exceptions.py
+++ exceptions.py
@@ -1,22 +1,22 @@
 class duplicate_label_found(Exception):
     def __init__(self, label):
-        Exception.__init__(self, 'Label "' + label + '" already defined')
+        Exception.__init__(self, 'Label "{0}" already defined'.format(label))
 
 class incorrect_number_of_parameters(Exception):
     def __init__(self, name, found, expected):
-        Exception.__init__(self, "Incorrect number of parameters for " + name + ", found " + found + " expected " + expected)
+        Exception.__init__(self, "Incorecct number of parameters for {0}, found {1} but expected {2}".format(name, found, expected))
 
 class invalid_parameter(Exception):
     def __init__(self):
         Exception.__init__(self, "Invalid parameter")
 
-class invalid_register_name(Expcetion):
+class invalid_register_name(Exception):
     def __init__(self, name):
-        Exception.__init__(self, "Invalid register name: " + name)
+        Exception.__init__(self, "Invalid register name: {0}".format(name))
 
 class multiple_label_definitions(Exception):
     def __init__(self, label_name):
-        Exception.__init__(self, 'Multiple definitions for label "' + label + '" found')
+        Exception.__init__(self, 'Multiple definitions for label "{0}" found'.format(label_name))
 
 class branch_out_of_range(Exception):
     def __init__(self):
@@ -24,4 +24,13 @@ class branch_out_of_range(Exception):
 
 class translate_num_out_of_range(Exception):
     def __init__(self, value, min_value, max_value):
-        Exception.__init__(self, "Translated number " + value + " not in range [" + min_value + ", " + max_value + "]")
+        Exception.__init__(self, "Translated number {0} not in range [{1}, {2}]".format(value, min_value, max_value))
+
+class translate_num_error(Exception):
+    def __init__(self, number):
+        Exception.__init__(self, "Error translating number {0}".format(number))
+
+class translate_inst_error(Exception):
+    def __init__(self, name, args):
+        Exception.__init__(self, "{0}".format(name + " " + " ".join(args)))
+
