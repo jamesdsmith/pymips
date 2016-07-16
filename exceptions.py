f62f418843cd26b4df475592263da065376fcb0f
James Smith <codefiend@gmail.com>
2016-07-16 12:16:50 -0700

    More exception definitions

--- exceptions.py
+++ exceptions.py
@@ -9,3 +9,19 @@ class incorrect_number_of_parameters(Exception):
 class invalid_parameter(Exception):
     def __init__(self):
         Exception.__init__(self, "Invalid parameter")
+
+class invalid_register_name(Expcetion):
+    def __init__(self, name):
+        Exception.__init__(self, "Invalid register name: " + name)
+
+class multiple_label_definitions(Exception):
+    def __init__(self, label_name):
+        Exception.__init__(self, 'Multiple definitions for label "' + label + '" found')
+
+class branch_out_of_range(Exception):
+    def __init__(self):
+        Exception.__init__(self, "Branch address out of range")
+
+class translate_num_out_of_range(Exception):
+    def __init__(self, value, min_value, max_value):
+        Exception.__init__(self, "Translated number " + value + " not in range [" + min_value + ", " + max_value + "]")
